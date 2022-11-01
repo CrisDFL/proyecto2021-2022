3e82d08bb46ff3addd589cd8874666ff35ed4422
Tihxuwu <93411477+CrisDFL@users.noreply.github.com>
2022-11-01 17:11:50 -0500

    nav 2.0

--- src/common/components/Nav/index.tsx
+++ src/common/components/Nav/index.tsx
@@ -2,52 +2,25 @@ import { FC } from 'react';
 import './style.css';
 
 const Nav: FC = () => (
-  <nav className="navbar navbar-expand-lg bg-dark">
-    <div className="container-fluid navigation">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a
-            className="nav-link active text-white"
-            aria-current="page"
-            href="/">
-            Inicio
-          </a>
-        </li>
-        <li className="nav-item">
-          <a
-            className="nav-link active text-white"
-            aria-current="page"
-            href="./Galeria">
-            Galería
-          </a>
-        </li>
-        <li className="nav-item">
-          <a
-            className="nav-link active text-white"
-            aria-current="page"
-            href="./Contactanos">
-            Contáctanos
-          </a>
-        </li>
-        <li className="nav-item">
-          <a
-            className="nav-link active text-white"
-            aria-current="page"
-            href="./Login">
-            Iniciar Sesión
-          </a>
-        </li>
-      </ul>
-      <div className="d-flex align-items-center">
-        <span className="text-white subtitle">BLACK AND YELLOW</span>
-        <img
-          src="/resources/img/header/logo.png"
-          width="50px"
-          height="50px"
-          alt="logo"
-        />
-      </div>
-    </div>
+  <nav className="navbar navbar-expand-lg ">
+    <label htmlFor="btn">
+
+    <input type="checkbox" className="button" id="btn"/>
+        <div className="toggle">
+            <span className="top_line common"></span>
+            <span className="middle_line common"></span>
+            <span className="bottom_line common"></span>
+        </div>
+    <span className="logo">
+      BLACK AND YELLOW
+    </span>
+    <ul>
+      <li><a className="active" href=".">Inicio</a></li>
+      <li><a href=".">Galeria</a></li>
+      <li><a href=".">Contactanos</a></li>
+      <li><a href=".">Iniciar Sesion</a></li>
+    </ul>
+    </label>
   </nav>
 );
 
