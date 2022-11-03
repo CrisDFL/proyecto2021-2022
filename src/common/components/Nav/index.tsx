a84f227287767899974ac08687e4d20fc2e3d120
CrisDFL <93411477+CrisDFL@users.noreply.github.com>
2022-11-02 20:12:58 -0500

    nav 3.0

--- src/common/components/Nav/index.tsx
+++ src/common/components/Nav/index.tsx
@@ -1,27 +1,59 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import './style.css';
 
-const Nav: FC = () => (
-  <nav className="navbar navbar-expand-lg ">
-    <label htmlFor="btn">
+const Nav: FC = () => {
+  const [active, setActive] = useState('nav__menu');
+  const [icon, setIcon] = useState('nav__toggler');
+  const navToggle = () => {
+    if (active === 'nav__menu') {
+      setActive('nav__menu nav__active');
+    } else setActive('nav__menu');
 
-    <input type="checkbox" className="button" id="btn"/>
-        <div className="toggle">
-            <span className="top_line common"></span>
-            <span className="middle_line common"></span>
-            <span className="bottom_line common"></span>
-        </div>
-    <span className="logo">
-      BLACK AND YELLOW
-    </span>
-    <ul>
-      <li><a className="active" href=".">Inicio</a></li>
-      <li><a href=".">Galeria</a></li>
-      <li><a href=".">Contactanos</a></li>
-      <li><a href=".">Iniciar Sesion</a></li>
-    </ul>
-    </label>
-  </nav>
-);
+    // Icon Toggler
+    if (icon === 'nav__toggler') {
+      setIcon('nav__toggler toggle');
+    } else setIcon('nav__toggler');
+  };
+  return (
+    <nav className="nav navbar navbar-expand-lg bg-dark">
+      <ul className={active}>
+        <li className="nav__item">
+          <a href="/" className="nav__link">
+            Inicio
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="./Galeria" className="nav__link">
+            Galería
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="./Contactanos" className="nav__link">
+            Contáctanos
+          </a>
+        </li>
+        <li>
+          <a href="./Login" className="nav__login">
+            Iniciar Sesión
+          </a>
+        </li>
+      </ul>
+      <div className="d-flex align-items-center">
+        <span className="text-white subtitle">BLACK AND YELLOW</span>
+        <img
+          src="/resources/img/header/logo.png"
+          width="50px"
+          height="50px"
+          alt="logo"
+        />
+      </div>
+      <div onClick={navToggle} className={icon}>
+        <div className="line1"></div>
+        <div className="line2"></div>
+        <div className="line3"></div>
+      </div>
+    </nav>
+  );
+};
 
 export default Nav;
